f646bb15e71cf1858bdd2385ebff3b3b4b15006d
Roberto Arias <walom.090@gmail.com>
2015-12-15 01:12:35 -0500

    Fixed form filler #4 - Now stops if resource is "yes" (multiple resources not supported yet)

--- form-fillers/form_filler_4.js
+++ form-fillers/form_filler_4.js
@@ -2,6 +2,11 @@ chrome.runtime.sendMessage({
 	"message": "give_me_data"
 },  function(response) {
 
-	document.getElementById("entry_1712191337").value = response.data.other_notes;
-	console.log("We're almost done!");
+	// Check if a resource was given
+	if (response.data.given_resource == "1")
+		console.log("Resources not supported yet");
+	else {
+		document.getElementById("entry_1712191337").value = response.data.other_notes;
+		console.log("We're almost done!");
+	}
 });
