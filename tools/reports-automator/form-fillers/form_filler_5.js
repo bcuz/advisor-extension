a3cf76f09ea9690ba6f47a4b0d1d1fac9e089167
Roberto Arias <walom.090@gmail.com>
2016-05-09 02:01:29 -0400

    Updated manifest to v0.62, added deployment script using google closure compiler and removed unused fillers

--- tools/reports-automator/form-fillers/form_filler_5.js
+++
@@ -1,7 +0,0 @@
-// This should be the confirmation page after a report was submitted
-console.log("This will be replaced with the check of the report that was submitted");
-
-// Notify that this report is successful
-chrome.runtime.sendMessage({
-	"message": "report-success"
-});
