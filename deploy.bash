f9c20fea8682e5c5c537bf84468acc6ad5fd361d
Adam Nawrocki <adam.nawroc@gmail.com>
2016-07-25 14:50:10 -0400

    new compiler location

--- deploy.bash
+++ deploy.bash
@@ -2,7 +2,7 @@
 ## BUILD SCRIPT ##
 
 # Replace this variable with the location to google closure compiler in your local environemnt
-GOOGLE_COMPILER='C:\Users\compiler-latest\compiler.jar'
+GOOGLE_COMPILER='C:\users\family\desktop\compiler-latest\compiler.jar'
 
 # Make sure deploy directory exists
 mkdir deploy 2>/dev/null
