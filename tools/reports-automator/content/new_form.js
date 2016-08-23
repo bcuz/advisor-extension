40d26514b6e80f248228aa8dc9610492aa3685cf
Adam Nawrocki <adam.nawroc@gmail.com>
2016-08-23 08:27:03 -0400

    make extension pick up that the course is react

--- tools/reports-automator/content/new_form.js
+++ tools/reports-automator/content/new_form.js
@@ -32,7 +32,7 @@ COURSES = {
 	"Python": ["python"],
 	"Command Line": ["command-line"],
 	"Sass": ["sass"],
-	"ReactJS": ['reactjs'],
+	"ReactJS": ['react'],
 	"NOT A CODE QUESTION": ["subscription_billing", "path"]
 }
 
