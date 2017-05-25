3c804d51b8e0c894d34c35a139d8d14748f453fe
Jake Brisk <jakebrisk@gmail.com>
2017-05-25 16:15:17 -0400

    Simplify invoicing for mentors

--- tools/invoice-generator/background/index.js
+++ tools/invoice-generator/background/index.js
@@ -5,18 +5,33 @@ chrome.runtime.onMessage.addListener((response, sender, sendResponse) => {
 	// Only run for invoice generation
 	if (response.message == "popup") {
 		if (response.id == "open-invoice") {
-			console.log("Opening invoice");
-			// Open tracksmart page and get records for the last 2 weeks
-			chrome.tabs.create({
-				url: 'https://timeclock.tracksmart.com/app/time/manage',
-				active: false
-				}, function(tab) {
-					chrome.tabs.executeScript(tab.id, {
-						file: "deploy/invoice_filler.js",
-						runAt: "document_end"
-					});
+			chrome.tabs.create({active: true, url: 'https://codecademy.wheniwork.com/payroll/'},
+				function(tab){
+					chrome.tabs.executeScript(tab.id, {file: 'libs/jquery-2.2.4.min.js'},
+						function(){
+							chrome.tabs.executeScript(tab.id, {file: 'deploy/create_invoice.js', runAt: 'document_end'});
+						}
+					);
 				}
 			);
+
+			console.log("Opening invoice");
+			// If also mentor, open tracksmart page and get records for the last 2 weeks
+			chrome.storage.sync.get({mentor: false}, function(data){
+				if(data.mentor){
+					chrome.tabs.create({
+						url: 'https://timeclock.tracksmart.com/app/time/manage',
+						active: false
+						}, function(tab) {
+							chrome.tabs.executeScript(tab.id, {
+								file: "deploy/invoice_filler.js",
+								runAt: "document_end"
+							});
+						}
+					);
+				}	
+			});
+				
 		}
 	}
 
